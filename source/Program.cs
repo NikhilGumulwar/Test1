62c984d3f4ac21b03fbd7b64ca88a418a3c0b7e7
MOTIFWORKS\Nikhil.gumulwar <nikhil.gumulwar@motifworks.com>
2016-09-15 17:04:00 +0530

    1st commit

---
+++ source/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("At last sync scuccess!!!!");
+            //Display Hello Git Hub
+            Console.WriteLine("Hello Git Hub");
+            Console.WriteLine("Hello from Sri");
+            // Keep the console window open in debug mode.
+            //Enterkey to exit
+
+            //Console.WriteLine("Name is:-" + p.Name);
+            Console.WriteLine("Press any key to exit.");
+			Console.WriteLine("Again Command Line Working ??????");
+            Console.ReadKey();
+            //Test 1
+            //Hellooooo guys
+
+            Console.WriteLine("Again Command Line Working ??????");
+        }
+    }
+}
+
